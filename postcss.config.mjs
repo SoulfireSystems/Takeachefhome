7d8bc6358bbacf88982889b640abaedc25053940
SoulfireSystems <honeypottev@gmail.com>
2025-12-11 15:00:54 +0000

    Fix PostCSS by using @tailwindcss/postcss plugin

---
+++ postcss.config.mjs
@@ -0,0 +1,9 @@
+// postcss.config.mjs
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
+const config = {
+  plugins: [tailwindcss, autoprefixer],
+};
+
+export default config;
